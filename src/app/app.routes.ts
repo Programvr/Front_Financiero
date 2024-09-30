4f7c14f8ec65b3703893905c4551aafd6ada732a
Marlon Valbuena <marlon_l_g@outlook.com>
2024-09-30 12:28:43 -0500

    Ajuste de rutas de servicios back y mejora eliminación de perfiles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { CrearPerfilComponent } from './crear-perfil/crear-perfil.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/CreacionPerfil', pathMatch: 'full' },
+  { path: '', redirectTo: 'CreacionPerfil', pathMatch: 'full' }, // Redirigir a 'CreacionPerfil'
   { path: 'CreacionPerfil', component: CrearPerfilComponent }
 ];
